d997a44122d2d7125aa47dd0d4964fd91f52084b
Lance Martin <ldmartin7777@gmail.com>
2016-05-24 15:41:52 -0500

    linked jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,24 @@
-var questions = {
-  question1: {
-    question: "What is not part of Donald Trump's healthcare plans?",
-    answers: ["<div id = 'wrong' class = 'row col-md-4'><p>Allowing tax payers to deduct their insurance premiums from their federal taxable income.</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>A total repeal  of obamacare.</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>Require transparency in pricing from healthcare providers.</p></div>","<div id = 'right' class = 'row col-md-4'><p>Establish a 2% American Health Improvement tax upon immigrant workers.</p></div>"]
-  },
-  question2: {
-    question: "Which Candidate is wishing to lower the corporate tax rate to 15%?",
-    answers: ["<div id = 'wrong' class = 'row col-md-4'><p></p></div>","<div id = 'wrong' class = 'row col-md-4'><p></p></div>","<div id = 'right' class = 'row col-md-4'><p></p></div>"],
-  },
-  question3: {
-    question: "which candidate wishes to ease unnecessary regulatory burdens on community banks, which provide credit to small business owners and families looking to invest in their futures?",
-    answers: ["<div id = 'wrong' class = 'row col-md-4'><p></p></div>","<div id = 'wrong' class = 'row col-md-4'><p></p></div>","<div id = 'right' class = 'row col-md-4'><p></p></div>"],
-  },
-  question4: {
-    question: "Which candidate is against the death penalty?",
-    answers: ["<div id = 'wrong' class = 'row col-md-4'><p></p></div>","<div id = 'wrong' class = 'row col-md-4'><p></p></div>","<div id = 'right' class = 'row col-md-4'><p></p></div>"]
-  }
-};
+$(document).ready(function(){
+  var questions = {
+    question1: {
+      question: "What is not part of Donald Trump's healthcare plans?",
+      answers: ["<div id = 'wrong' class = 'row col-md-4'><p>Allowing tax payers to deduct their insurance premiums from their federal taxable income.</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>A total repeal  of obamacare.</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>Require transparency in pricing from healthcare providers.</p></div>","<div id = 'right' class = 'row col-md-4'><p>Establish a 2% American Health Improvement tax upon immigrant workers.</p></div>"]
+    },
+    question2: {
+      question: "Which Candidate is wishing to lower the corporate tax rate to 15%?",
+      answers: ["<div id = 'wrong' class = 'row col-md-4'><p>Bernie Sanders</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>Hillary Clinton</p></div>","<div id = 'right' class = 'row col-md-4'><p>Donald Trump</p></div>"],
+      right: "<img src = 'assets/images/seriousTrump.jpg'>"
+    },
+    question3: {
+      question: "which candidate wishes to ease unnecessary regulatory burdens on community banks, which provide credit to small business owners and families looking to invest in their futures?",
+      answers: ["<div id = 'wrong' class = 'row col-md-4'><p>Donald Trump</p></div>","<div id = 'right' class = 'row col-md-4'><p>Hillary Clinton</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>Bernie Sanders</p></div>"],
+      right: "<img src = 'assets/images/hillary.jpg'>"
+    },
+    question4: {
+      question: "Which candidate is against the death penalty?",
+      answers: ["<div id = 'right' class = 'row col-md-4'><p>Bernie Sanders</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>Hillary Clinton</p></div>","<div id = 'wrong' class = 'row col-md-4'><p>Donald Trump</p></div>"],
+      right: "<img src = 'assets/images/bernie.jpg'>"
+      
+    }
+  };
+});
